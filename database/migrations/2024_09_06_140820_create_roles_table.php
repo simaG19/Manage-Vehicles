6157e08de5ad71227e912c2163fb4ceefe906ed3
simon <simonwubshet254@gmail.com>
2024-09-07 13:02:02 +0300

    roles_table

---
+++ database/migrations/2024_09_06_140820_create_roles_table.php
@@ -0,0 +1,22 @@
+<?php
+// database/migrations/xxxx_xx_xx_create_roles_table.php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique(); // 'admin', 'driver'
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
