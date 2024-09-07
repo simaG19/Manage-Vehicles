201d556220c964b0d444f996a2deef3312496675
simon <simonwubshet254@gmail.com>
2024-09-07 13:00:23 +0300

    CheckRole of user

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $role
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if (Auth::check() && Auth::user()->role->name === $role) {
+            return $next($request);
+        }
+
+        // Redirect or return forbidden response
+        return response()->json(['error' => 'Forbidden'], 403);
+    }
+}
